777bc5971a9523c5b63684c101409bfaaaa3d852
Antonytm <antontishchenko@gmail.com>
2026-03-18 02:30:10 +0200

    feat: temporarily extra files

---
+++ examples/basic-astro/.sitecore/component-map.ts
@@ -0,0 +1,7 @@
+// Below are built-in components that are available in the app, it's recommended to keep them as is
+
+export const componentMap = new Map<string, any>([
+
+]);
+
+export default componentMap;
